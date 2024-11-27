afad2938f702075fd520e6392d8897283f782c0f
Ubuntu <ubuntu@ip-172-31-30-108.ec2.internal>
2024-11-27 07:00:24 +0000

    Fixed setup_project.sh

--- setup_project.sh
+++ setup_project.sh
@@ -6,23 +6,26 @@ if (($? == 0)); then
 	# Conda environment does not exist
 	conda create -n preble python=3.10
 	echo "Conda environment created"
+	conda activate preble
+
+	echo "Installing PyPI dependencies..."
+	pip3 install -e .
+	echo "PyPi dependencies installed"
+
+	echo "Installing local version of SGLang..."
+	pip install -e "python[all]"
+	echo "Local version of SGLang installed"
+
+	echo "Installing FlashInfer..."
+	pip install flashinfer -i https://flashinfer.ai/whl/cu124/torch2.4/
+	echo "FlashInfer installed"
+
+	conda deactivate
 fi
 
-if [[ "$(CONDA_DEFAULT_ENV)" != "preble" ]]; then
+if [[ "$CONDA_DEFAULT_ENV" != "preble" ]]; then
 	echo "Activate conda environment is not preble"
 	echo "Activating preble environment..."
 	conda activate preble
 	echo "Preble environment activated"
 fi
-
-echo "Installing PyPI dependencies..."
-pip3 install -e .
-echo "PyPi dependencies installed"
-
-echo "Installing local version of SGLang..."
-pip install -e "python[all]"
-echo "Local version of SGLang installed"
-
-echo "Installing FlashInfer..."
-pip install flashinfer -i https://flashinfer.ai/whl/cu124/torch2.4/
-echo "FlashInfer installed"
